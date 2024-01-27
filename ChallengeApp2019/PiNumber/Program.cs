031d289a3af2eeb28d7df3bff49879207490e7c0
iMedia SysteM <125789515+imediasystem@users.noreply.github.com>
2024-01-27 20:30:26 +0100

    Program oblicza pierwiastek kwadratowy z liczby PI

--- ChallengeApp2019/PiNumber/Program.cs
+++ ChallengeApp2019/PiNumber/Program.cs
@@ -7,8 +7,8 @@ namespace PiNumber
         static void Main(string[] args)
         {
             Console.WriteLine("Program wyświetla stałą PI z zadaną dokładnością.");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Pi = {0:#.#####}.", Math.PI);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("PI = {0:#.#####}.", Math.PI);
             Console.ResetColor();
         }
     }
---
+++ ChallengeApp2019/SqrtPI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Math;
+namespace SqrtPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program wyświetla pierwiastek kwadratowy z liczyby PI");
+            Console.WriteLine("z dokładnością do dwóch miejsc po przecinku.");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Sqrt(pi) = {0:#.##}.", Sqrt(PI));
+            Console.ResetColor();
+        }
+    }
+}
