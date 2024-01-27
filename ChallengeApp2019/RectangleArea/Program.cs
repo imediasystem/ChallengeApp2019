8dee834fd4dd075a7d0dc9285eda72b8bc78a0df
iMedia SysteM <125789515+imediasystem@users.noreply.github.com>
2024-01-27 19:16:34 +0100

    Program na pole prostokąta

---
+++ ChallengeApp2019/RectangleArea/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, pole;
+
+            Console.WriteLine("Program oblicza pole prostokąta.");
+            Console.WriteLine("Podaj bok a.");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj bok b.");
+            b = double.Parse(Console.ReadLine());
+
+            pole = a * b;
+
+            Console.Write("Pole prostokąta o boku a = {0} i boku b = {1}", a, b);
+            Console.WriteLine(" wynosi {0}.", pole);
+        }
+    }
+}
