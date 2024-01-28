f2b5b4600f293014ec04dc51c2434e2e9213ccd2
iMedia SysteM <125789515+imediasystem@users.noreply.github.com>
2024-01-28 13:45:35 +0100

    Program przelicza stopnie Fahrenheita na stopnie Celsiusa

---
+++ ChallengeApp2019/FahrenheitCelsiusRecalculation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FahrenheitCelsiusRecalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double F, C;
+                        
+            Console.WriteLine("Podaj temperaturę w stopniach Fahrenheita");
+            F = double.Parse(Console.ReadLine());
+            C = 5d / 9d * (F - 32);
+            Console.WriteLine("Temperatura w stopniach Celsjusza wynosi {1:##}", F, C);
+        }
+    }
+}
